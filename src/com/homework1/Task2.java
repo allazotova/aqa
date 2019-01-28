493f7073baf28b2a6429b41853454bac1246d048
Alla <alla@mbp-alla.synapse.com>
2019-01-28 20:57:29 +0200

    made changes

--- src/com/homework1/Task2.java
+++ src/com/homework1/Task2.java
@@ -3,21 +3,17 @@ package com.homework1;
 public class Task2 {
 
     public static void main(String[] args) {
+
         int varInt = 24234234;
         double varDouble;
         String varString;
 
         varString= String.valueOf(varInt);
-        System.out.println("String value is: "+ varString);
 
         varDouble = Double.parseDouble(varString);
-        System.out.println(varDouble);
 
         varInt =  (int) varDouble;
-        System.out.println(varInt);
+        System.out.format("String: %s, Double: %f, Integer: %d",varString,varDouble,varInt);
 
     }
-
-
-
 }
